5c3fb08bef9070de46b38b31aa5b42ba135c309c
shane-sepeda <shane.sepeda@icloud.com>
2022-08-21 20:18:18 -0500

    Added js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var FormEl = document.querySelector("#form");
+var InputEl = document.querySelector("#search");
+var ButtonsEl = document.querySelector("#buttons");
+var CardTopEl = document.querySelector("#card-top");
+
+var cities=[];
+
